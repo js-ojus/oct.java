06334af5213ab34c56275ddb786f28c310d33fb9
JONNALAGADDA Srinivas <js@ojuslabs.com>
2013-05-28 19:30:49 +0530

    More complete `core.Atom` and `core.AtomInReaction`.
    * Most of the state of an atom needed has been represented by the
      above classes.

--- oct/src/com/ojuslabs/oct/core/AtomInReaction.java
+++ oct/src/com/ojuslabs/oct/core/AtomInReaction.java
@@ -11,13 +11,21 @@ package com.ojuslabs.oct.core;
  * AtomInReaction extends the class {@link Atom} with additional properties and
  * methods needed for its participation in reactions.
  */
-public class AtomInReaction {
-    // The underlying atom that is wrapped.
+public final class AtomInReaction {
+    // The underlying atom that is wrapped. The interface of this atom is NOT
+    // wrapped. Rather, the atom itself is made available as a public member.
     public final Atom atom;
     // Direct correspondence with an atom in the goal molecule.
     private int       _goalAtomId;
     // Correspondence with an atom in the immediate product.
     private int       _productAtomId;
+    // Correspondence with an atom in a reactant.
+    private Molecule  _reactant;
+    private int       _reactantAtomId;
+    // Should the chirality of this atom be preserved throughout?
+    private boolean   _preserveChirality;
+    // Should this atom be prevented from getting modified anywhere at all?
+    private boolean   _dontTouch;
 
     /**
      * Initialisation of a new atom.
